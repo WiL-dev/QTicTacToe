94510da049d9b8d986c2ccd56730e1b10bd2dc81
William Ardila <wfabian.ardila@gmail.com>
2023-06-28 23:04:22 +1200

    refactor: Adjust the winning logic to 3x3 board

--- QTicTacToe.Api/Models/Board.cs
+++ QTicTacToe.Api/Models/Board.cs
@@ -1,12 +1,26 @@
 namespace QTicTacToe.Api.Models;
 
 public class Board {
-    private readonly string[] cubes = new string[3*3*3];
+
+    // private readonly string[] cubes = new string[3*3*3];
+    private readonly string[] grid = new string[3*3];
+
+    private static readonly int[][] lines = new int[8][]{
+        new int[3] {0, 1, 2},
+        new int[3] {3, 4, 5},
+        new int[3] {6, 7, 8},
+        new int[3] {0, 3, 6},
+        new int[3] {1, 4, 7},
+        new int[3] {2, 5, 8},
+        new int[3] {0, 4, 8},
+        new int[3] {2, 4, 6}
+    };
 
     public MoveOutput MakeMove(MoveInput move)
     {
+        int cubePos = move.CubePos;
 
-        if(cubes[move.CubePos] is not null)
+        if(grid[cubePos] is not null)
         {
             return new MoveOutput
             {
@@ -14,22 +28,38 @@ public class Board {
             };
         }
 
-        if (IsWinner(move))
-        {
-            return new MoveOutput {
-                isValid = true,
-                IsWinner = true
-            };
-        }
+        grid[cubePos] = move.Mark;
 
-        return new MoveOutput
+        MoveOutput moveOutput = new MoveOutput
         {
             isValid = true
         };
+
+        if (IsWinner(move))
+        {
+            moveOutput.IsWinner = true;
+        }
+
+        return moveOutput;
     }
 
-    private static bool IsWinner(MoveInput move)
+    private bool IsWinner(MoveInput move)
     {
-        return move.CubePos == 13;
+        IEnumerable<int[]> filteredLines = from line in lines
+            where line.Contains(move.CubePos)
+            select line;
+
+        foreach (int[] line in filteredLines)
+        {
+            if (line.All(cubePos => grid[cubePos] == move.Mark))
+            {
+                Console.WriteLine("WIN");
+                return true;
+            }
+        }
+
+        Console.WriteLine(string.Join(" - ", grid));
+
+        return false;
     }
 }
--- QTicTacToe.Api/Models/MoveInput.cs
+++ QTicTacToe.Api/Models/MoveInput.cs
@@ -5,6 +5,7 @@ public class MoveInput
     public int X { get; set; }
     public int Y { get; set; }
     public int Z { get; set; }
+    public required string Mark { get; set; }
 
     public int CubePos => (X + Y*3 + Z*9);
 
