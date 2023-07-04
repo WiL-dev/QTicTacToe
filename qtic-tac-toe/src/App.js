8189e9c87db5a44962e8411a876f850a5b4bd712
William Ardila <wfabian.ardila@gmail.com>
2023-07-05 00:42:42 +1200

    feat: Add three library and scaffold the three component

--- qtic-tac-toe/src/App.js
+++ qtic-tac-toe/src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
+
 import './App.css';
+
+function Cube() {
+  const refContainer = useRef();
+  const [renderer, setRenderer] = useState();
+
+  useEffect(() => {
+    const { current: container } = refContainer;
+    if (container && !renderer) {
+      const containerWidth = container.clientWidth;
+      const containerHeight = container.clientHeight;
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      const render = new THREE.WebGLRenderer();
+      render.setSize(containerWidth, containerHeight);
+      setRenderer(render);
+
+      container.appendChild(render.domElement);
+    }
+  }, []);
+
+  return (
+    <div
+      className="App-cube"
+      ref={refContainer}
+    >
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          QTicTacToe.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/WiL-dev/QTicTacToe"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github repo
         </a>
+      <Cube />
       </header>
     </div>
   );
