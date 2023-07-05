bfdadbc31bf0d106318e2fa30880e7c7dcad6270
William Ardila <wfabian.ardila@gmail.com>
2023-07-05 19:53:13 +1200

    feat: Add board mockup

--- qtic-tac-toe/src/App.js
+++ qtic-tac-toe/src/App.js
@@ -1,29 +1,50 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import './App.css';
 
 function Cube() {
   const refContainer = useRef();
-  const [renderer, setRenderer] = useState();
+  const refRenderer = useRef();
 
   useEffect(() => {
     const { current: container } = refContainer;
+    const { current: renderer } = refRenderer;
+
     if (container && !renderer) {
       const containerWidth = container.clientWidth;
       const containerHeight = container.clientHeight;
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(
         75,
-        window.innerWidth / window.innerHeight,
+        containerWidth / containerHeight,
         0.1,
         1000
       );
       const render = new THREE.WebGLRenderer();
       render.setSize(containerWidth, containerHeight);
-      setRenderer(render);
+      refRenderer.current = render;
 
       container.appendChild(render.domElement);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const board = new THREE.Mesh(geometry, material);
+      scene.add(board);
+
+      camera.position.z = 1.7;
+      const controls = new OrbitControls(camera, render.domElement);
+      const target = new THREE.Vector3(0.1, 0.1, 0.1);
+      controls.target = target;
+
+      function animate() {
+        requestAnimationFrame(animate);
+        // board.rotation.x += 0.01;
+        // board.rotation.y += 0.005;
+        render.render(scene, camera);
+      }
+      animate();
     }
   }, []);
 
